f93122695edf05c889d22f7e2e7670664dc1bbdd
khuongbhan <hbk32@drexel.edu>
2020-07-06 01:32:17 -0400

    mobile quote

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,43 @@
 
+var $burger = $("#burger-icon");
+var $nav = $('nav[data-nav]');
+var $navA = $('nav[data-nav] a');
+var isVisible = false;
+
+function mouseClick(){
+
+    console.log("click");
+    if(isVisible === false){
+        $nav.show();
+        isVisible = true;
+    }
+    else{
+        $nav.hide();
+        isVisible = false;
+    }
+}
+
+function myFunction(x) {
+    x.classList.toggle("change");
+
+    console.log("click");
+    if(isVisible === false){
+        $nav.show();
+        isVisible = true;
+    }
+    else{
+        $nav.hide();
+        isVisible = false;
+    }
+}
+
+$navA.on( "click", myFunction);
+$burger.on( "click", myFunction);
+
+
+// $burger.on( "click", mouseClick);
+// // close the mobile menu when menu link is clicked
+// $navA.on( "click", mouseClick);
+
+
+
