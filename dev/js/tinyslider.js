014b601c9f0ba1b0b879a318b5a1982c1f16be74
khuongbhan <hbk32@drexel.edu>
2020-07-12 20:46:13 -0400

    tinyslider

--- dev/js/tinyslider.js
+++ dev/js/tinyslider.js
@@ -8,7 +8,18 @@ tns(
     "edgePadding": 50,
     "mouseDrag": true,
     controlsPosition: "bottom",
-    navPosition: "bottom"
+    navPosition: "bottom",
+    "responsive": {
+        "350": {
+            "items": 1
+        },
+        "768": {
+            "items": 1
+        },
+        "1440": {
+            "items": 2
+        }
+    }
     // slideBy: 'page',
     // autoplay: true
 }
