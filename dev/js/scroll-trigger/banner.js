481bffc04fa55176cffa472770ca119c277cf6d9
khuongbhan <hbk32@drexel.edu>
2020-08-16 04:09:18 -0400

    banner animation completed

--- dev/js/scroll-trigger/banner.js
+++ dev/js/scroll-trigger/banner.js
@@ -17,6 +17,7 @@ bannerAboutTimeLine.play();
 
 var aboutIntroductionsTimeLine = gsap.timeline();
 var intro = 1;
+var slow = 2;
 
 gsap.set("#about-para-1", {transformOrigin:"top"});
 gsap.set("#about-para-1", {transformOrigin:"top"});
@@ -52,3 +53,61 @@ bannerIndexTimeLine.from("#index-title", {duration: pageLoads, alpha:0, yPercent
 
 
 bannerIndexTimeLine.play();
+
+/* ========================
+    Hindsight Banner
+======================== */
+
+var bannerHindsightTimeLine = gsap.timeline();
+
+bannerHindsightTimeLine.from("#hindsight-title", {duration: pageLoads, alpha: 0, xPercent: -50}, "title")
+                       .from("#hindsight-slash", {duration: pageLoads, alpha: 0, rotation: -270, xPercent: 100}, "slash")
+
+bannerHindsightTimeLine.play();
+
+var hindsightQuoteTimeLine = gsap.timeline();
+
+hindsightQuoteTimeLine.from("#hindsight-planet", {duration: slow, alpha: 0, xPercent: 100, yPercent: 100}, "planet")
+                      .from("#hindsight-mark", {duration: slow, alpha: 0, xPercent: -100, yPercent: -100}, "planet")
+                      .from("#hindsight-box", {duration: slow, alpha: 0, xPercent: -300}, "planet")
+                      .from("#hindsight-text", {duration: slow, alpha: 0, yPercent: -100}, "planet")
+
+
+ScrollTrigger.create({
+    animation: hindsightQuoteTimeLine,
+    toggleActions: "play none none none",
+    trigger: "#hindsight-title",
+    start: "top 13%",
+    end: "top 60%",
+    // markers: true,
+    // scrub: 1,
+    id: "hindsight-intro"
+});
+
+
+/* ========================
+    Wonders Banner
+======================== */
+
+var bannerWondersTimeLine = gsap.timeline();
+
+bannerWondersTimeLine.from("#wonders-title", {duration: slow, alpha: 0, xPercent: -50}, "title")
+                     .from("#wonders-slash", {duration: slow, alpha: 0, rotation: -270, xPercent: 100}, "title")
+                    //  .to("#wonders-box", {duration: slow, alpha: 1}, "title")
+                     .from("#wonders-text", {duration: slow, alpha: 0, xPercent: 100}, "title")
+                     
+                     
+bannerWondersTimeLine.play();
+
+
+/* ========================
+    Friends Banner
+======================== */
+
+var bannerFriendsTimeLine = gsap.timeline();
+
+gsap.set("#friends-slash", {transformOrigin: "center"});
+
+bannerFriendsTimeLine.from("#friends-slash", {duration: slow, alpha: 0, rotation: 90}, "slash")
+                     .from("#friends-section-title", {duration: slow, alpha: 0, yPercent: 10}, "title")
+                     .from("#friends-text", {duration: slow, alpha: 0, yPercent: -10}, "title")
